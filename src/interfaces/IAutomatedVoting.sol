bf8dc029f37c3948823d7e1cab6fcf162814a724
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-08-04 16:12:53 -0400

    👷 add errors

--- src/interfaces/IAutomatedVoting.sol
+++ src/interfaces/IAutomatedVoting.sol
@@ -22,6 +22,25 @@ interface IAutomatedVoting {
 
     // Errors
 
+    /// @notice emitted when the caller is not the elite council
+    error CallerNotEliteCouncil();
+
+    /// @notice emitted when the caller is not staked
+    error CallerNotStaked();
+
+    /// @notice emitted when the election is not ready to be finalized
+    error ElectionNotReadyToBeFinalized();
+
+    /// @notice emitted when the election is not ready to be started
+    error ElectionNotReadyToBeStarted();
+
+    /// @notice emitted when an elite council member cannot step down
+    /// this could happen if everyone tries to step down at once
+    error EliteCouncilMemberCannotStepDown();
+
+    /// @notice emitted when the caller has already voted for this election
+    error AlreadyVoted();
+
     // View Functions
 
     /// @notice returns the time until the next scheduled election
