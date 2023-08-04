c1ff137237a4b6c3f4d18a4ea2b72171c13b7114
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-08-04 10:18:02 -0400

    👷 finalize election

--- src/interfaces/IAutomatedVoting.sol
+++ src/interfaces/IAutomatedVoting.sol
@@ -11,11 +11,17 @@ interface IAutomatedVoting {
     /// @notice returns the time until the next scheduled election
     function timeUntilNextScheduledElection() external view returns (uint256);
 
+    /// @notice returns the time until the election state ends
+    /// @param election the election to check
     function timeUntilElectionStateEnd(uint256 election) external view returns (uint256);
 
     // Mutative Functions
 
     /// @notice start the 6 month election cycle
     function startScheduledElection() external;
+
+    /// @notice finalize the election
+    /// @param election the election to finalize
+    function finalizeElection(uint256 election) external;
 
 }
