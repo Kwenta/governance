ed30ea8da3ea2757f959cc1b05002c50b9b662cb
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-08-16 09:49:24 -0400

    👷 add check for nonexistent elections to timeUntilElectionStateEnd

--- src/AutomatedVoting.sol
+++ src/AutomatedVoting.sol
@@ -68,7 +68,8 @@ contract AutomatedVoting is IAutomatedVoting {
     function timeUntilElectionStateEnd(
         uint256 _election
     ) public view override returns (uint256) {
-        if (elections[_election].isFinalized) {
+        /// @dev if the election is finalized or the election number is greater than the number of elections, return 0
+        if (elections[_election].isFinalized || _election >= electionNumbers.length) {
             return 0;
         } else {
             return elections[_election].endTime - block.timestamp;
