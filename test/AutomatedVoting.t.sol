374173454faff6c2686f5c7198d4a37a4719f9e8
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-09-14 18:28:54 -0400

    ✅ testSortCandidatesPositionStaysAtIndex0

--- test/AutomatedVoting.t.sol
+++ test/AutomatedVoting.t.sol
@@ -958,6 +958,30 @@ contract AutomatedVotingTest is DefaultStakingV2Setup {
 
     // _sortCandidates()
 
+    function testSortCandidatesPositionStaysAtIndex0() public {
+        vm.warp(block.timestamp + 21 weeks);
+        automatedVotingInternals.startScheduledElection();
+        fundAccountAndStakeV2(user1, 1);
+        vm.startPrank(user1);
+        automatedVotingInternals.nominateMultipleCandidates(1, council);
+        
+        assertEq(automatedVotingInternals.getCandidateAddress(1, 0), user1);
+        assertEq(automatedVotingInternals.getCandidateAddress(1, 1), user2);
+        assertEq(automatedVotingInternals.getCandidateAddress(1, 2), user3);
+        assertEq(automatedVotingInternals.getCandidateAddress(1, 3), user4);
+        assertEq(automatedVotingInternals.getCandidateAddress(1, 4), user5);
+
+        address[] memory result = automatedVotingInternals.sortCandidates(1, user1, 1);
+
+        /// @dev make sure voting didnt change the order
+        assertEq(result[0], user1);
+        assertEq(result[1], user2);
+        assertEq(result[2], user3);
+        assertEq(result[3], user4);
+        assertEq(result[4], user5);
+
+    }
+
     // _cancelOngoingElections()
 
     function testCancelOngoingElectionsStepDown() public {
